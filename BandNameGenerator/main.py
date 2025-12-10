532d66ab75e19bc9f69f636a391fe320d981755f
Mika <mika.juhani.vaisanen@icloud.com>
2025-12-10 21:05:16 +0200

    A program that generates a band name by concatenating two input strings

---
+++ BandNameGenerator/main.py
@@ -0,0 +1,8 @@
+﻿# Generate a band name by concatenating two input strings
+
+print("Welcome to the Band Name Generator.")
+
+city_name = input("What´s the name of the city you grew up in?\n")
+pet_name = input("What´s your pet´s name?\n")
+
+print("Your band could be called " + city_name + " " + pet_name)
